22384dd9e8512972f156f21d42544fecc57ec88e
Psychoman500 <martin.sutek@gmail.com>
2021-10-15 12:02:18 +0200

    Devide total pieces of cake and total amount of people and if it is equal or bigger than mount of pieces for each than it is true.

--- Open-Lab-03.07/Divider.cs
+++ Open-Lab-03.07/Divider.cs
@@ -6,7 +6,14 @@ namespace Open_Lab_03._07
     {
         public bool EqualSlices(int total, int people, int each)
         {
-            throw new NotImplementedException();
+            if (total / people >= each )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
